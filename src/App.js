788e1028e3d41b21e320fccb1353bcbe2e2a930f
xiedanyan <xiedanyan@skieer.com>
2022-11-24 20:09:13 +0800

    update ci

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Github CI3
+          Learn Github CI4
         </a>
       </header>
     </div>
